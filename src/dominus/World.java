995d22e9f34503345a69489a8554977722ab5826
ennetws <ennetws@ac18d2ed-0344-0410-9524-d112d406ba35>
2008-01-19 18:10:14 +0000

    Testing comments.

--- src/dominus/World.java
+++ src/dominus/World.java
@@ -1,5 +1,15 @@
 package dominus;
 
+/**
+ * “The most incomprehensible thing about the world is 
+ * that it is at all comprehensible.”
+ * 
+ * This class represent everything including the render 
+ * engine and the physics engine.
+ * 
+ * @author ibraheem
+ *
+ */
 public class World {
 
 }
