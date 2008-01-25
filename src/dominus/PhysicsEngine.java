00a0495b378df68c39fccf8671a36ad3a62e09fd
chrisherz007 <chrisherz007@ac18d2ed-0344-0410-9524-d112d406ba35>
2008-01-25 20:06:27 +0000

    added blank RenderEngine and PhysicEngine classes.

---
+++ src/dominus/PhysicsEngine.java
@@ -0,0 +1,12 @@
+package dominus;
+
+/**
+ * PhysicsEngine contains all physic related methods (gravity, projectile motion, etc)
+ * 
+ * @author cherz
+ *
+ */
+
+public class PhysicsEngine {
+
+}
---
+++ src/dominus/RenderEngine.java
@@ -0,0 +1,12 @@
+package dominus;
+
+/**
+ * The RenderEngine class will render all elements within the World
+ * 
+ * @author cherz
+ *
+ */
+
+public class RenderEngine {
+
+}
