31b0c7b4687827c57ee4b154cd6e7b512bc9efb2
ennetws <ennetws@ac18d2ed-0344-0410-9524-d112d406ba35>
2008-01-27 19:41:43 +0000

    Added the constructor.

--- src/dominus/Element2D.java
+++ src/dominus/Element2D.java
@@ -1,14 +1,15 @@
 package dominus;
 
 /**
- * 2D element class for menus, text, object selection, etc
+ * 2D element class for menus, text, object selection, etc.
  * 
  * @author cherz
  *
  */
 
 public class Element2D extends Element {
-
 	
-	
+	public Element2D(String iden){
+		super(iden);
+	}
 }
